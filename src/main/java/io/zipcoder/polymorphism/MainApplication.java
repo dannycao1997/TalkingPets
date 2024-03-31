701833d4443e3511a4e81fa9a7e7eb7aa4cb88aa
Danny Cao <dannycao1997@yahoo.com>
2024-03-30 22:30:49 -0400

    part 1

--- src/main/java/io/zipcoder/polymorphism/MainApplication.java
+++ src/main/java/io/zipcoder/polymorphism/MainApplication.java
@@ -1,4 +1,12 @@
 package io.zipcoder.polymorphism;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MainApplication {
+    public static void Main(String[] args){
+        Scanner sc = new Scanner(System.in); // scanner object to get user input
+        List<Pet> pets = new ArrayList<>(); // array list to store my pets
+    }
 }
